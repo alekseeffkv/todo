da462bc38f46ca02795a50f7fb16c57964c1715b
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-11-17 11:16:13 +0300

    Add Title

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,18 @@
 import { FC } from 'react';
+import Button from '../button';
 import './app.less';
 
-const App: FC = () => <div />;
+const App: FC = () => {
+  return (
+    <main>
+      <div className="container">
+        <h1>Список задач</h1>
+        <div>
+          <Button buttonProps={{ type: 'button' }}>Добавить задачу</Button>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default App;
